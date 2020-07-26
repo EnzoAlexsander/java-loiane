30fbc2714ade694abbe84f01e28ef891eab59131
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-26 20:49:35 -0300

    Operadores

---
+++ basico/aula06/operadores/exercicios/Ex06_CalcularRaioCirculo.java
@@ -0,0 +1,17 @@
+package basico.aula06.operadores.exercicios;
+
+import java.util.Scanner;
+
+public class Ex06_CalcularRaioCirculo {
+    public static void main(String[] args) {
+        // 6- Faça um programa que peça o raio de um círculo, calcule e mostre sua área
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Raio do círculo: ");
+        double raio = scan.nextDouble();
+
+        double area = Math.PI * Math.pow(raio, 2);
+
+        System.out.println("A área do círculo é: " + area);
+    }
+}
