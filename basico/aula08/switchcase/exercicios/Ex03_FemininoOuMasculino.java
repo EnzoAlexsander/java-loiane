6f7eb44814d2d7e846d9b9a6978076bf22442b54
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-31 13:24:16 -0300

    Switch Case

---
+++ basico/aula08/switchcase/exercicios/Ex03_FemininoOuMasculino.java
@@ -0,0 +1,23 @@
+package basico.aula08.switchCase.exercicios;
+
+import java.util.Scanner;
+
+public class Ex03_FemininoOuMasculino {
+    public static void main(String[] args) {
+        //3- Faça um programa que verifique se uma letra digitada é "F" ou "M".
+        // Conforme a letra escrever: F-Feminino, M-Masculino, Sexo inválido.
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira o seu sexo(F ou M): ");
+        String sexo = scan.next();
+
+        if (sexo.equalsIgnoreCase("F")) {
+            System.out.println("F-Feminino");
+        } else if (sexo.equalsIgnoreCase("M")) {
+            System.out.println("M-Masculino");
+        } else {
+            System.out.println("Sexo Inválido");
+        }
+    }
+}
