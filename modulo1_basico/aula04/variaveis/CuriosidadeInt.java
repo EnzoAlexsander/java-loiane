0f49aa3643f31e8d7f6a10acc65974427df010c2
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-01 00:10:47 -0300

    Variáveis

---
+++ modulo1_basico/aula04/variaveis/CuriosidadeInt.java
@@ -0,0 +1,11 @@
+package modulo1_basico.aula04.variaveis;
+
+public class CuriosidadeInt {
+    public static void main(String[] args) {
+
+        int var1 = 2147483647; //valor máximo do int
+        int var2 = 100;
+
+        System.out.println(var1 + var2);
+    }
+}
---
+++ modulo1_basico/aula04/variaveis/VariaveisChar.java
@@ -0,0 +1,12 @@
+package modulo1_basico.aula04.variaveis;
+
+public class VariaveisChar {
+    public static void main(String[] args) {
+
+        char o = 'o';
+        char i = 'i';
+        char interrogacao = 0X00E1;
+
+        System.out.println("" + o + i + interrogacao);
+    }
+}
