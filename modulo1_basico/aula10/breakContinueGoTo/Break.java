c98181809a35c3f84eae8c873e58660b2e75b045
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-04 00:02:45 -0300

    Break

---
+++ modulo1_basico/aula10/breakContinueGoTo/Break.java
@@ -0,0 +1,25 @@
+package modulo1_basico.aula10.breakContinueGoTo;
+
+import java.util.Scanner;
+
+public class Break {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num;
+        int max;
+
+        System.out.print("Digite um número: ");
+        num = scan.nextInt();
+
+        System.out.print("Digite um número limite: ");
+        max = scan.nextInt();
+
+        for (int i = num; i <= max; i++) {
+            if (i % 7 == 0) {
+                System.out.println("O valor de i é: " + i);
+                break;
+            }
+        }
+    }
+}
