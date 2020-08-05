f09feed76539c2ac9b9b898a2f52b296821ce561
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-05 01:02:49 -0300

    Arrays

---
+++ modulo1_basico/aula11/arraysVetores/exercicios/Ex04_VetorRaizQuadrada.java
@@ -0,0 +1,35 @@
+package modulo1_basico.aula11.arraysVetores.exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Ex04_VetorRaizQuadrada {
+    public static void main(String[] args) {
+        //4- Criar um vetor A com 15 elementos inteiros. Construir um vetor B de mesmo tamanho, sendo que cada elemento
+        // do vetor B deverá ser a raiz quadrada do respectivo elemento de A, ou seja: B[i] = sqrt(A[i]).
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[15];
+        double[] vetorB = new double[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print("Informe o valor da posição " + i + ": ");
+            vetorA[i] = scan.nextInt();
+            vetorB[i] = Math.sqrt(vetorA[i]);
+        }
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        DecimalFormat df = new DecimalFormat("###,###.###");
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(df.format(vetorB[i]) + " ");
+        }
+    }
+}
---
+++ modulo1_basico/aula11/arraysVetores/exercicios/Ex06_SomaVetores.java
@@ -0,0 +1,47 @@
+package modulo1_basico.aula11.arraysVetores.exercicios;
+
+import java.util.Scanner;
+
+public class Ex06_SomaVetores {
+    public static void main(String[] args) {
+        //6- Criar dois vetores A e B cada um com 10 elementos inteiros. Construir um vetor C, onde cada elemento de C
+        // é a soma dos respectivos elementos em A e B, ou seja: C[i] = A[i] + B[i].
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+        int[] vetorC = new int[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print("Informe o valor do vetorA da posição " + i + ": ");
+            vetorA[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print("Informe o valor do vetorB da posição " + i + ": ");
+            vetorB[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < vetorC.length; i++) {
+            vetorC[i] = vetorA[i] + vetorB[i];
+        }
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor C = ");
+        for (int i = 0; i < vetorC.length; i++) {
+            System.out.print(vetorC[i] + " ");
+        }
+    }
+}
