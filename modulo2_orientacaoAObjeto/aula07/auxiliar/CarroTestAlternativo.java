72e9895ab6721e650faf398015f846367f7dacdb
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-17 20:57:40 -0300

    Private Public

---
+++ modulo2_orientacaoAObjeto/aula07/auxiliar/CarroTestAlternativo.java
@@ -0,0 +1,11 @@
+package modulo2_orientacaoAObjeto.aula07.auxiliar;
+
+import modulo2_orientacaoAObjeto.aula07.privatepublic.Carro;
+
+public class CarroTestAlternativo {
+    public static void main(String[] args) {
+
+        Carro carro = new Carro();
+        carro.marca = "Fiat";
+    }
+}
