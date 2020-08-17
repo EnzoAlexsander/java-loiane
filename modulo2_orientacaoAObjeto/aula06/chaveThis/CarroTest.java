7ea6576599f96a97a199f92effba75aa56619978
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-16 22:30:53 -0300

    Chave This

---
+++ modulo2_orientacaoAObjeto/aula06/chaveThis/CarroTest.java
@@ -0,0 +1,9 @@
+package modulo2_orientacaoAObjeto.aula06.chaveThis;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro van = new Carro("Fiat", "Ducato");
+
+        System.out.println(van.numPassageiros);
+    }
+}
