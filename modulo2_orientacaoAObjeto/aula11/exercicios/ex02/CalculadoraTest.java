5c663598f099fef51c0f1172df1171c9aff807bf
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-21 23:46:51 -0300

    Recursividade

---
+++ modulo2_orientacaoAObjeto/aula11/exercicios/ex02/CalculadoraTest.java
@@ -0,0 +1,8 @@
+package modulo2_orientacaoAObjeto.aula11.exercicios.ex02;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+
+        System.out.println(Calculadora.somatorio(5));
+    }
+}
