3c475bfe09375156ee16985a48202d4d2b95f91a
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-23 00:03:48 -0300

    Herança

---
+++ modulo2_orientacaoAObjeto/aula13/heranca/Test.java
@@ -0,0 +1,23 @@
+package modulo2_orientacaoAObjeto.aula13.heranca;
+
+public class Test {
+    public static void main(String[] args) {
+
+        /*
+        Aluno aluno = new Aluno();
+        Professor prof = new Professor();
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Enzo");
+        prof.setNome("Quezado");
+        */
+
+        Pessoa aluno = new Aluno();
+        //aluno.get
+
+        Pessoa professor = new Professor();
+        //prof.get
+
+
+    }
+}
