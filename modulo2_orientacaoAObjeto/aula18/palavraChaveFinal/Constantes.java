fb54f7d780cc21dc3f2a1e328d0e9fbd22f7c0c5
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-08-29 22:20:04 -0300

    Palavra Chave Final

---
+++ modulo2_orientacaoAObjeto/aula18/palavraChaveFinal/Constantes.java
@@ -0,0 +1,7 @@
+package modulo2_orientacaoAObjeto.aula18.palavraChaveFinal;
+
+public final class Constantes {
+
+    public static final String URL_BLOG = "http://enzo.com";
+    public static final String CURSO_COMPLETO = "http://enzo.training.com";
+}
