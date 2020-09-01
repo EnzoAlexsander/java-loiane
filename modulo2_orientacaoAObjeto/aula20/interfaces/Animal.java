c50f8764de9d82d7d5d420ebdddd7e5784280049
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-09-01 16:57:34 -0300

    Interfaces

---
+++ modulo2_orientacaoAObjeto/aula20/interfaces/Animal.java
@@ -0,0 +1,16 @@
+package modulo2_orientacaoAObjeto.aula20.interfaces;
+
+public abstract class Animal {
+
+    private String nome;
+
+    public abstract void emitirSom();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ modulo2_orientacaoAObjeto/aula20/interfaces/AnimalDomesticado.java
@@ -0,0 +1,12 @@
+package modulo2_orientacaoAObjeto.aula20.interfaces;
+
+public interface AnimalDomesticado {
+
+    public final int ANO = 2016;
+
+    void imprimirNome();
+
+    void levarVeterinario();
+
+    void alimentar();
+}
