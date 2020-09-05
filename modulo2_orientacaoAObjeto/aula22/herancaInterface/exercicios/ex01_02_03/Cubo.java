ce3f9a38448b15b572e8e0dc0eeb15c783d79b4a
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-09-04 21:22:08 -0300

    Herança Interface

---
+++ modulo2_orientacaoAObjeto/aula22/herancaInterface/exercicios/ex01_02_03/Cubo.java
@@ -0,0 +1,24 @@
+package modulo2_orientacaoAObjeto.aula22.herancaInterface.exercicios.ex01_02_03;
+
+public class Cubo extends Figura3D {
+
+    private int lado;
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return 6 * (lado * lado);
+    }
+
+    @Override
+    public double calcularVolume() {
+        return lado * lado * lado;
+    }
+}
