e2a8836d46666321530bfaad1e0c760fd0db8eed
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-09-10 14:23:45 -0300

    Enumeradores

---
+++ modulo3_intermediario/aula01/enumeradores/metodosAbstratos/TipoDocumento.java
@@ -0,0 +1,19 @@
+package modulo3_intermediario.aula01.enumeradores.metodosAbstratos;
+
+public enum TipoDocumento {
+
+    CPF {
+        @Override
+        public String geraNumeroTeste() {
+            return GeraCpfCnpj.cpf();
+        }
+    },
+    CNPJ {
+        @Override
+        public String geraNumeroTeste() {
+            return GeraCpfCnpj.cnpj();
+        }
+    };
+
+    public abstract String geraNumeroTeste();
+}
