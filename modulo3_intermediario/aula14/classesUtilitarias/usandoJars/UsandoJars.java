61b6b247dce414ea4cc65ed0ab30e132bb426dfe
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-09-19 13:06:53 -0300

    Classes Utilitárias

---
+++ modulo3_intermediario/aula14/classesUtilitarias/usandoJars/UsandoJars.java
@@ -0,0 +1,10 @@
+package modulo3_intermediario.aula14.classesUtilitarias.usandoJars;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+public class UsandoJars {
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("enzo"));
+    }
+}
